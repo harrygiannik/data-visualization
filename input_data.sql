4fdb2530fa95f3bcca14db84c7cb6c0d57954fab
harrygiannik <harrygiannik@gmail.com>
2020-05-31 18:58:40 +0300

    tidy up repo

--- input_data.sql
+++
@@ -1,9 +0,0 @@
-use world_bank_data;
-LOAD DATA local INFILE 'data-visualization/csv_data/output.csv' INTO TABLE data
-FIELDS terminated by ',';
-LOAD DATA local INFILE 'data-visualization/csv_data/data_3_years_avg.csv' INTO TABLE data_3_years_avg
-FIELDS terminated by ',';
-LOAD DATA local INFILE 'data-visualization/csv_data/data_5_years_avg.csv' INTO TABLE data_5_years_avg
-FIELDS terminated by ',';
-LOAD DATA local INFILE 'data-visualization/csv_data/data_10_years_avg.csv' INTO TABLE data_10_years_avg
-FIELDS terminated by ',';
